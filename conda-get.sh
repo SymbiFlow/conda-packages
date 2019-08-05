0941436008f1a2e69193ebe54667de0541defb43
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-08-05 13:59:01 -0700

    Do not update conda, it breaks it.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- conda-get.sh
+++ conda-get.sh
@@ -5,13 +5,13 @@ set -e
 
 CONDA_PATH=${1:-~/conda}
 
-wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod a+x Miniconda3-latest-Linux-x86_64.sh
+CONDA_FILE=Miniconda3-4.5.12-Linux-x86_64.sh
+wget -c https://repo.continuum.io/miniconda/$CONDA_FILE
+chmod a+x $CONDA_FILE
 if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
-        ./Miniconda3-latest-Linux-x86_64.sh -p $CONDA_PATH -b -f
+        ./$CONDA_FILE -p $CONDA_PATH -b -f
 fi
 export PATH=$CONDA_PATH/bin:$PATH
-conda update -y conda
 if [ ! -z "$CONDA_BUILD_VERSION" ]; then
 	conda install -y conda-build==$CONDA_BUILD_VERSION
 	echo "conda-build==$CONDA_BUILD_VERSION" > $CONDA_PATH/conda-meta/pinned
